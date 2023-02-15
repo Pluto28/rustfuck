5f652cf9259a5d67e2ee3807e146f1258e596c84
Pluto28 <mendax103@gmail.com>
2023-02-15 10:45:57 -0300

    lexer stuff

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Token {
     OBRACKETS,
     CBRACKETS,
@@ -39,5 +39,58 @@ impl Tokenize for String {
         }
 
         tokens
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_obrackets() {
+        let data = String::from("[").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::OBRACKETS);
+    }
+
+    #[test]
+    fn test_cbrackets() {
+        let data = String::from("]").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::CBRACKETS);
+    }
+
+    #[test]
+    fn test_incmemptr() {
+        let data = String::from(">").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::INCMEMPTR);
+    }
+
+    #[test]
+    fn test_decmemptr() {
+        let data = String::from("<").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::DECMEMPTR);
+    }
+
+    #[test]
+    fn test_incval() {
+        let data = String::from("+").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::INCVAL);
+    }
+
+    #[test]
+    fn test_decval() {
+        let data = String::from("-").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::DECVAl);
+    }
+
+    #[test]
+    fn test_getval() {
+        let data = String::from(",").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::GETVAl);
+    }
+
+    #[test]
+    fn test_putval() {
+        let data = String::from(".").to_tokens().pop().unwrap();
+        assert_eq!(data, Token::PUTVAL);
     }
 }
